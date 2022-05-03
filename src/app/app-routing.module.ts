0d77cca614c3c1ebfa4e0c99ed0329921ad23093
junior rocco <norbertorocco@hotmail.com>
2022-05-03 20:17:03 -0300

    primeiro commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,58 @@
+<<<<<<< HEAD
+import { ResultadoCreateComponent } from './components/views/resultado/resultado-create/resultado-create.component';
+import { ResultadoReadComponent } from './components/views/resultado/resultado-read/resultado-read.component';
+import { HomeComponent } from './components/views/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'resultados',
+    component: ResultadoReadComponent
+  },
+  {
+    path: 'resultados/create',
+    component: ResultadoCreateComponent
+  }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+=======
+import { ResultadoCreateComponent } from './components/views/resultado/resultado-create/resultado-create.component';
+import { ResultadoReadComponent } from './components/views/resultado/resultado-read/resultado-read.component';
+import { HomeComponent } from './components/views/home/home.component';
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'resultados',
+    component: ResultadoReadComponent
+  },
+  {
+    path: 'resultados/create',
+    component: ResultadoCreateComponent
+  },
+  
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+>>>>>>> 454d9d6402a09cdcc540e0fe9b41617fe567e6b9
